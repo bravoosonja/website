8156132b7018eb00ac8d9cbef72283234b062660
Bravoo Sonja <bravoosonja@gmail.com>
2022-07-09 18:41:48 +1000

    Add CMS Prismic

---
+++ .slicemachine/assets/slices/TextBlock/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TextBlock';
+
+export default {
+  title: 'slices/TextBlock'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text_block","items":[],"primary":{"title":[{"type":"heading1","text":"Reintermediate e-business blockchains","spans":[]}],"description":[{"type":"paragraph","text":"Nulla reprehenderit cillum cillum excepteur reprehenderit. Nisi laborum ullamco aliquip et dolor cupidatat cupidatat aute ad nisi.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,28 @@ import "../styles/globals.scss";
 import Layout from "../components/Layout";
 import CustomCursor from "../components/feature/CustomCursor";
 import CursorContextProvider from "../context/cursor-context";
+// prismic
+import { PrismicProvider } from "@prismicio/react";
+import { PrismicPreview } from "@prismicio/next";
+import { linkResolver, repositoryName } from "../prismicio";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <CustomCursor />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props }) => (
+        <Link href={href}>
+          <a {...props}>{children}</a>
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <CustomCursor />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PrismicPreview>
+    </PrismicProvider>
   );
 }
 
