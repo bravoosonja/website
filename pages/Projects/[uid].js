a904df94e9a2cf88a3b56466c2636fd4f4dfd219
Bravoo Sonja <bravoosonja@gmail.com>
2022-07-10 12:03:20 +1000

    Add individual project page

--- pages/Projects/[uid].js
+++ pages/Projects/[uid].js
@@ -1,42 +1,50 @@
 import { useRouter } from "next/router";
-import { createClient, linkResolver } from "../../prismicio";
-import * as prismicH from "@prismicio/helpers";
+//import { createClient, linkResolver } from "../../prismicio";
+//import * as prismicH from "@prismicio/helpers";
 // styles
 import styles from "../../styles/components/projectLayout.module.scss";
 
-export default function ProjectPage({ page }) {
-  // const router = useRouter();
-  // const { uid } = router.query;
+export default function ProjectPage() {
+  const router = useRouter();
+  const { uid } = router.query;
 
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles.title}>
-          <h1>{page.uid}</h1>
+          <h1>{uid}</h1>
         </div>
       </div>
     </div>
   );
 }
 
-export async function getStaticProps({ params, previewData }) {
-  const client = createClient({ previewData });
-
-  const page = await client.getByUID("page", params.uid);
-
+export async function getStaticProps() {
+  const res = await fetch("/public/data.js");
+  const data = await res;
   return {
-    props: { page },
+    props: {}, // will be passed to the page component as props
   };
 }
 
-export async function getStaticPaths() {
-  const client = createClient();
-  const pages = await client.getAllByType("page", { lang: "*" });
+// export async function getStaticProps({ params, previewData }) {
+//   const client = createClient({ previewData });
 
-  return {
-    paths: pages.map((page) => {
-      return { params: { uid: page.uid } };
-    }),
-    fallback: false, // false or 'blocking'
-  };
-}
+//   const page = await client.getByUID("page", params.uid);
+
+//   return {
+//     props: { page },
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   const client = createClient();
+//   const pages = await client.getAllByType("page", { lang: "*" });
+
+//   return {
+//     paths: pages.map((page) => {
+//       return { params: { uid: page.uid } };
+//     }),
+//     fallback: false, // false or 'blocking'
+//   };
+// }
